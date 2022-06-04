9f4ceb306d9fa08883269a1ffac50fb39ba112a5
Thoumey <thomas.borgelin@gmail.com>
2022-06-04 04:13:09 +0300

    react to role setter message

---
+++ initialReact.js
@@ -0,0 +1,12 @@
+let rolesChannel = client.channels.cache.get('982405191309619230');
+rolesChannel.messages.fetch(`982444567158751262`).then(message => {
+    message.react(':beeangery:699593773553811476');
+    message.react(':OwOmen:982378343812968468');
+    message.react(':Angery:982354770780110878');
+    message.react(':Borpagun:982378359306723348');
+    message.react(':nobuild:982378288800473165');
+    message.react(':Facepalm:982378269330526238');
+    message.react(':peepoparty:982378256814702603');
+    message.react(':trumpW:261529841545904130');
+    message.react('😡');
+});
